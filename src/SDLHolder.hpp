f87292c2b9ef895642890a325c3ccccbd45d7993
Robert <robert.d.bruce@icloud.com>
2024-06-14 11:48:10 -0700

    hello

---
+++ src/SDLHolder.hpp
@@ -0,0 +1,27 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+// SDL Init Object
+class SDLHolder
+{
+	public:
+		//The window we'll be rendering to
+		SDL_Window* gWindow = NULL;
+
+		//The window renderer
+		SDL_Renderer* gRenderer = NULL;
+
+		//Globally used font
+		TTF_Font* gFont = NULL;
+
+		//Initializes variables
+		SDLHolder(const int wFromMain, const int hFromMain);
+
+		//Deallocates memory
+		~SDLHolder();
+};
+
