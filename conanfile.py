f4aba5dd1cbfde5655f4471309903fc2643dde6f
Robert Bruce <robert.d.bruce@icloud.com>
2024-06-20 15:28:44 -0700

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -7,9 +7,8 @@ class ExampleRecipe(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("sdl/[~2.30]")
-        # self.requires("sdl_ttf/2.0.18")
-        # self.requires("sdl_image/2.0.5")
+        self.requires("sdl_ttf/[~2.22]")
+        # self.requires("sdl_image/[~2.6]")
 
     def layout(self):
         cmake_layout(self)
